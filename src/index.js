1662e730d061209d0b59a42534c6d325e1ffed49
sandilokaa <sandyloka790@gmail.com>
2025-01-30 00:21:39 +0700

    docs: add shop router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 import Home from "./pages/home/Home";
+import Shop from "./pages/shop/Shop";
 import DetailItem from "./pages/item/DetailItem";
 
 const roots = document.getElementById("root");
@@ -15,6 +16,7 @@ root.render(
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
         <Route path="/item/detail/:id" element={<DetailItem />}></Route>
       </Routes>
     </Provider>
